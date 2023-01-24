8e3924150458bcdcf17eb8c8c98637af1b6fa189
Muhammed Yakubu <m.yakubu@mail.utoronto.ca>
2023-01-24 17:25:36 -0500

    add FAILED as a status type

--- src/shared/messages/KVMessage.java
+++ src/shared/messages/KVMessage.java
@@ -11,7 +11,8 @@ public interface KVMessage {
 		PUT_UPDATE, 	/* Put - request successful, i.e. value updated */
 		PUT_ERROR, 		/* Put - request not successful */
 		DELETE_SUCCESS, /* Delete - request successful */
-		DELETE_ERROR 	/* Delete - request successful */
+		DELETE_ERROR, 	/* Delete - request successful */
+		FAILED			/* Failed - message format unknown, message size exceeded, etc */
 	}
 
 	/**
