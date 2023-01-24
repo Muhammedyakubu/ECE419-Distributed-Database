5c6a153b00108b46bcb9454a9ac8c419d5225637
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2023-01-23 21:38:23 -0500

    started implementation of KVStore. Taking some time to understand how the similar interfaces work in echoClient

--- src/client/KVStore.java
+++ src/client/KVStore.java
@@ -1,9 +1,31 @@
 package client;
 
+import org.apache.log4j.Logger;
 import shared.messages.KVMessage;
+import shared.communication.CommunicationModule;
+import shared.messages.Message;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Looking through echoClient & echoServer, I realize that receiveMessage and
+ * sendMessage can be implemented in a separate class. But the similarities
+ * between the two are so few that I don't think it's worth it.
+ */
 public class KVStore implements KVCommInterface {
 
+	private static Logger logger = Logger.getRootLogger();
+	private Socket clientSocket;
+
+	private static final int BUFFER_SIZE = 1024;
+	private static final int DROP_SIZE = 128 * BUFFER_SIZE;
+	private InputStream input;
+	private OutputStream output;
+	private String address;
+	private int port;
 
 
 	/**
@@ -13,11 +35,14 @@ public class KVStore implements KVCommInterface {
 	 */
 	public KVStore(String address, int port) {
 		// TODO Auto-generated method stub
+		this.address = address;
+		this.port = port;
 	}
 
 	@Override
 	public void connect() throws Exception {
 		// TODO Auto-generated method stub
+		clientSocket = new Socket(address, port);
 	}
 
 	@Override
@@ -35,5 +60,72 @@ public class KVStore implements KVCommInterface {
 	public KVMessage get(String key) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public void sendMessage(Message msg) throws IOException {
+		byte[] msgBytes = msg.toByteArray();
+		output.write(msgBytes, 0, msgBytes.length);
+		output.flush();
+//        TODO: separate logging for server and client
+		logger.info("Send message:\t '" + msg.toString() + "'");
+
+	}
+	private Message receiveMessage() throws IOException {
+
+		int index = 0;
+		byte[] msgBytes = null, tmp = null;
+		byte[] bufferBytes = new byte[BUFFER_SIZE];
+
+		/* read first char from stream */
+		byte read = (byte) input.read();
+		boolean reading = true;
+
+		while(/*read != 13  && */ read != 10 && read !=-1 && reading) {/* CR, LF, error */
+			/* if buffer filled, copy to msg array */
+			if(index == BUFFER_SIZE) {
+				if(msgBytes == null){
+					tmp = new byte[BUFFER_SIZE];
+					System.arraycopy(bufferBytes, 0, tmp, 0, BUFFER_SIZE);
+				} else {
+					tmp = new byte[msgBytes.length + BUFFER_SIZE];
+					System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+					System.arraycopy(bufferBytes, 0, tmp, msgBytes.length,
+							BUFFER_SIZE);
+				}
+
+				msgBytes = tmp;
+				bufferBytes = new byte[BUFFER_SIZE];
+				index = 0;
+			}
+
+			/* only read valid characters, i.e. letters and constants */
+			bufferBytes[index] = read;
+			index++;
+
+			/* stop reading is DROP_SIZE is reached */
+			if(msgBytes != null && msgBytes.length + index >= DROP_SIZE) {
+				reading = false;
+			}
+
+			/* read next char from stream */
+			read = (byte) input.read();
+		}
+
+		if(msgBytes == null){
+			tmp = new byte[index];
+			System.arraycopy(bufferBytes, 0, tmp, 0, index);
+		} else {
+			tmp = new byte[msgBytes.length + index];
+			System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+			System.arraycopy(bufferBytes, 0, tmp, msgBytes.length, index);
+		}
+
+		msgBytes = tmp;
+
+		/* build final String */
+		Message msg = new Message(msgBytes);
+//        TODO: separate logging for server and client
+		logger.info("Receive message:\t '" + msg.toString() + "'");
+		return msg;
 	}
 }
