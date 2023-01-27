d1d63617717e7c051cc299529bd3a148a3a112be
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2023-01-27 02:05:15 -0500

    implemented cache interface & wrote stub functions for the different cache strategies

---
+++ src/app_kvServer/cache/LFUCache.java
@@ -0,0 +1,53 @@
+package app_kvServer.cache;
+
+public class LFUCache implements Cache{
+
+    public LFUCache(int cacheSize) {
+
+    }
+
+    /**
+     * @param key
+     * @return
+     */
+    @Override
+    public boolean contains(String key) {
+        return false;
+    }
+
+    /**
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public String getKV(String key) throws Exception {
+        return null;
+    }
+
+    /**
+     * @param key
+     * @param value
+     * @throws Exception
+     */
+    @Override
+    public void putKV(String key, String value) throws Exception {
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    @Override
+    public void evictKV() throws Exception {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void clearCache() {
+
+    }
+}
