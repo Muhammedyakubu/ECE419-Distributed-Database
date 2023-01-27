1324b1408bcefcdaa3b84a1eb3592c752e37ad25
Jasnoor Guliani <jasnoorguliani@Jasnoors-MacBook-Air.local>
2023-01-27 01:36:12 -0500

    Creates interface for persistent storage system classes

---
+++ src/database/KVdatabase.java
@@ -0,0 +1,13 @@
+package database;
+
+
+/**
+ * Class for manipulating key-value store database
+ * Used by kvServer to place key-value pairs in storage and retrieve values
+ *
+ * Implemented using log structured file and hashmap of key-address locations
+ */
+public class database {
+
+
+}
