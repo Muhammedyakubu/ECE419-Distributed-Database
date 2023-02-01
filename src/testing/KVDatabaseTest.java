2fa45e8262e408ae311c9820a89a0229e96fdc9f
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2023-01-31 19:48:15 -0500

    fixed existing db tests to run consistently

--- src/testing/KVDatabaseTest.java
+++ src/testing/KVDatabaseTest.java
@@ -2,26 +2,32 @@ package testing;
 
 import database.KVdatabase;
 import junit.framework.TestCase;
+import org.apache.log4j.BasicConfigurator;
 
 
 public class KVDatabaseTest extends TestCase{
 
+    KVdatabase db = new KVdatabase();
+    public void setUp() {
+        BasicConfigurator.configure();
+        db = new KVdatabase();
+    }
+
+    public void tearDown() {
+        db.clearStorage();
+    }
+
     public void testInsert() {
-        KVdatabase db = new KVdatabase();
         String key = "foo";
         String value = "bar";
         boolean expected = true;
         boolean actual = db.insertPair(key, value);
         assertEquals(expected, actual);
-
-
-
-
     }
 
 
     public void testGet() {
-        KVdatabase db = new KVdatabase();
+        db.insertPair("foo", "bar");
         String key = "foo";
         String expected = "bar";
         String actual = db.getValue(key);
@@ -29,7 +35,7 @@ public class KVDatabaseTest extends TestCase{
     }
 
     public void testDelete() {
-        KVdatabase db = new KVdatabase();
+        db.insertPair("foo", "bar");
         String key = "foo";
         boolean expected = true;
         boolean actual = db.deletePair(key);
@@ -37,7 +43,6 @@ public class KVDatabaseTest extends TestCase{
     }
 
     public void testUpdate() {
-        KVdatabase db = new KVdatabase();
         String key = "foo";
         db.insertPair(key, "bar");
         db.insertPair(key, "yeah");
@@ -47,7 +52,6 @@ public class KVDatabaseTest extends TestCase{
     }
 
     public void testClear() {
-        KVdatabase db = new KVdatabase();
         String key = "foo";
         String key2 = "foo2";
         db.insertPair(key, "bar");
