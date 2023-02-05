a9cf7764636d278421dc6e1808ff9536955ab282
Jasnoor Guliani <Jasnoor>
2023-02-05 16:29:39 -0500

    Deleted redundant additional tests file{

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -34,8 +34,7 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite clientSuite = new TestSuite("Basic Storage ServerTest-Suite");
 		clientSuite.addTestSuite(ConnectionTest.class);
-		clientSuite.addTestSuite(InteractionTest.class); 
-		clientSuite.addTestSuite(AdditionalTest.class);
+		clientSuite.addTestSuite(InteractionTest.class);
 		clientSuite.addTestSuite(KVMessageTest.class);
 		clientSuite.addTestSuite(KVServerTest.class);
 		clientSuite.addTestSuite(KVDatabaseTest.class);
