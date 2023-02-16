079870456e05ce3980627f9c2e480fea993cc941
Muhammed Yakubu <m.yakubu@mail.utoronto.ca>
2023-02-16 16:10:11 -0500

    initialize repo for m2, jars build :)

---
+++ src/app_kvECS/ECSClient.java
@@ -0,0 +1,73 @@
+package app_kvECS;
+
+import java.util.Map;
+import java.util.Collection;
+
+import ecs.IECSNode;
+
+public class ECSClient implements IECSClient {
+
+    @Override
+    public boolean start() {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public boolean stop() {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public boolean shutdown() {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public IECSNode addNode(String cacheStrategy, int cacheSize) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Collection<IECSNode> addNodes(int count, String cacheStrategy, int cacheSize) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Collection<IECSNode> setupNodes(int count, String cacheStrategy, int cacheSize) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public boolean awaitNodes(int count, int timeout) throws Exception {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public boolean removeNodes(Collection<String> nodeNames) {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public Map<String, IECSNode> getNodes() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public IECSNode getNodeByKey(String Key) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO
+    }
+}
---
+++ src/ecs/IECSNode.java
@@ -0,0 +1,25 @@
+package ecs;
+
+public interface IECSNode {
+
+    /**
+     * @return  the name of the node (ie "Server 8.8.8.8")
+     */
+    public String getNodeName();
+
+    /**
+     * @return  the hostname of the node (ie "8.8.8.8")
+     */
+    public String getNodeHost();
+
+    /**
+     * @return  the port number of the node (ie 8080)
+     */
+    public int getNodePort();
+
+    /**
+     * @return  array of two strings representing the low and high range of the hashes that the given node is responsible for
+     */
+    public String[] getNodeHashRange();
+
+}
