20048187b6a680b278c39d6601ae7a7877651ac5
Hope Hadfield <hopehadfield@Hopes-MacBook-Pro.local>
2023-02-27 11:24:15 -0500

    Implemented ECS CL parameters. Need to update once ECS initalization method is written

--- src/app_kvECS/ECSClient.java
+++ src/app_kvECS/ECSClient.java
@@ -1,9 +1,15 @@
 package app_kvECS;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.Collection;
 
+import app_kvServer.KVServer;
 import ecs.IECSNode;
+import logger.LogSetup;
+import org.apache.log4j.Level;
 
 public class ECSClient implements IECSClient {
 
@@ -67,7 +73,137 @@ public class ECSClient implements IECSClient {
         return null;
     }
 
+    private static Level StringToLevel(String levelString) {
+
+        if(levelString.equals(Level.ALL.toString())) {
+            return Level.ALL;
+        } else if(levelString.equals(Level.DEBUG.toString())) {
+            return Level.DEBUG;
+        } else if(levelString.equals(Level.INFO.toString())) {
+            return Level.INFO;
+        } else if(levelString.equals(Level.WARN.toString())) {
+            return Level.WARN;
+        } else if(levelString.equals(Level.ERROR.toString())) {
+            return Level.ERROR;
+        } else if(levelString.equals(Level.FATAL.toString())) {
+            return Level.FATAL;
+        } else if(levelString.equals(Level.OFF.toString())) {
+            return Level.OFF;
+        } else {
+            return null;
+        }
+    }
+
+    public static String parseCommandLine(String[] args, boolean run_ECS){
+        try {
+            if (args.length == 0) {
+                System.out.println("Error! Missing port number!");
+                System.out.println("Usage: java -jar m2-ecs.jar " +
+                        "-p <port number> -a <address> -l <logPath> -ll <logLevel> !");
+                return "Invalid";
+            }
+            if(args[0].equals("-h")){
+                System.out.println("Usage: java -jar m2-ecs.jar " +
+                        "-p <port number> -a <address> -l <logPath> -ll <logLevel> !");
+                return "Help printed.";
+            }
+            //WRONG ARGUMENT ENTRY
+            if(args.length % 2 != 0){
+                System.out.println("Error! Invalid entry of arguments!");
+                System.out.println("Usage: java -jar m2-ecs.jar " +
+                        "-p <port number> -a <address> -l <logPath> -ll <logLevel> !");
+                return "Invalid";
+                //System.exit(0);
+            }
+
+            int port_num = -1;
+            boolean port_present = false;
+            String address = "localhost";
+            String logPath = "logs/ecs.log";
+            String logLevel = " "; //DEFAULT IS SET TO ALL LATER
+
+            for(int i = 0; i < args.length; i++) {
+                //PORT CHECK
+                if(args[i].equals("-p")) {
+                    port_num = Integer.parseInt(args[i+1]);
+                    if(port_num < 0 || port_num > 65535){
+                        System.out.println("Error! Port number out of range!");
+                        System.out.println("Port number must fall between 0 and 65535, inclusive.");
+                        System.exit(0);
+                    }
+                    port_present = true;
+                }
+
+                //ADDRESS CHECK
+                if(args[i].equals("-a")) {
+                    address = args[i + 1];
+                }
+
+                //LOGPATH CHECK
+                if(args[i].equals("-l")) {
+                    logPath = args[i+1];
+                }
+
+                //LOGLEVEL CHECK
+                if(args[i].equals("-ll")) {
+                    logLevel = args[i+1];
+                }
+            }
+
+            if(port_present == false) {
+                System.out.println("Error! No port number found!");
+                System.out.println("Usage: java -jar m2-ecs.jar " +
+                        "-p <port number> -a <address> -l <logPath> -ll <logLevel> !");
+                return("No port, invalid");
+                //System.exit(0);
+            }
+
+            //WILL THROW UNKNOWN HOST EXCEPTION IF ADDRESS IS INVALID
+            InetAddress bind_address = InetAddress.getByName(address);
+
+            Level level = Level.ALL;
+
+            if(!logLevel.equals(" ")){
+                level = StringToLevel(logLevel);
+
+                if(level == null){
+                    System.out.println("Given loglevel was invalid. Set to default (ALL).");
+                    level = Level.ALL;
+                }
+            }
+
+            //WILL THROW I/O EXCEPTION IF PATH IS INVALID
+            if(run_ECS) {
+                new LogSetup(logPath, level);
+
+                //LAUNCH ECS HERE WITH SPECIFIED PORT AND ADDRESS!
+            }
+
+            String returned = "Port: " + port_num + " Address: " + address +
+                    " Logpath: " + logPath + " Loglevel: " + logLevel;
+            return returned;
+
+        } catch (IOException e) {
+            if(e instanceof UnknownHostException){
+                System.out.println("Error! Invalid address!");
+            } else {
+                System.out.println("Error! Unable to find logPath!");
+            }
+            System.out.println("Usage: java -jar m2-ecs.jar " +
+                    "-p <port number> -a <address> -l <logPath> -ll <logLevel> !");
+            return "Invalid";
+            //e.printStackTrace();
+            //System.exit(1);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Error! Invalid argument <port>! Not a number!");
+            System.out.println("Usage: java -jar m2-ecs.jar " +
+                    "-p <port number> -a <address> -l <logPath> -ll <logLevel> !");
+            return "Invalid";
+            //System.exit(1);
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO
+        parseCommandLine(args, true);
     }
 }
