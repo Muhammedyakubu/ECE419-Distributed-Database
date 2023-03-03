df83fcbc22522867d6f66733bf9ed999ddfbafae
Hope Hadfield <hopehadfield@Hopes-MacBook-Pro.local>
2023-03-03 11:18:28 -0500

    Trying to get interaction test to work

--- src/testing/RangeTest.java
+++ src/testing/RangeTest.java
@@ -19,7 +19,7 @@ public class RangeTest extends TestCase {
         BigInteger start = end.add(BigInteger.ONE);
         range.updateRange(start, end);
         BigInteger test = getHash("test");
-        assertTrue(range.inRange(test));
+        assertTrue(range.inRange(test) && range.inRange(start) && range.inRange(end));
     }
 
     @Test
@@ -43,5 +43,16 @@ public class RangeTest extends TestCase {
         boolean test3 = range.inRange(BigInteger.ZERO);
         boolean test4 = range.inRange(FFFF);
         assertTrue(test1 && test2 && test3 && test4);
+    }
+
+    @Test
+    public void testInRangeSpecific(){
+
+        BigInteger end = new BigInteger("98715326301568607101067940198791688124", 16);
+        BigInteger start = new BigInteger("180293372416575166452018847750613510822", 16);
+        BigInteger hash = new BigInteger("105490587539310211567724693302530339539", 16);
+        range.updateRange(start, end);
+        boolean test1 = range.inRange(hash);
+        assertFalse(test1 /*&& test2 && test3 && test4*/);
     }
 }
