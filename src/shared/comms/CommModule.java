4d3a9c5c817619517f2437a4938a7647479a7907
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2023-04-11 18:12:00 -0400

    synchronized send/receivemessage based on input stream so each thread reads a complete message

--- src/shared/comms/CommModule.java
+++ src/shared/comms/CommModule.java
@@ -29,16 +29,19 @@ public final class CommModule {
      * @throws IOException some I/O error regarding the output stream
      */
     public static void sendMessage(KVMessage msg, Socket socket) throws IOException {
+        // synchronize on socket to prevent concurrent access
         OutputStream output = socket.getOutputStream();
+        synchronized (output) {
 
-        byte[] msgBytes = msg.toByteArray();
-        output.write(msgBytes, 0, msgBytes.length);
-        output.flush();
-        if(msg.getStatus() != IKVMessage.StatusType.WAGWAN) {
-            logger.debug("SEND \t<"
-                    + socket.getInetAddress().getHostAddress() + ":"
-                    + socket.getPort() + ">: '"
-                    + msg + "'");
+            byte[] msgBytes = msg.toByteArray();
+            output.write(msgBytes, 0, msgBytes.length);
+            output.flush();
+            if(msg.getStatus() != IKVMessage.StatusType.WAGWAN) {
+                logger.debug("SEND \t<"
+                        + socket.getInetAddress().getHostAddress() + ":"
+                        + socket.getPort() + ">: '"
+                        + msg + "'");
+            }
         }
     }
 
@@ -49,75 +52,78 @@ public final class CommModule {
      * @throws IOException some I/O error regarding the input stream
      */
     public static KVMessage receiveMessage(Socket socket) throws IOException {
+        // synchronize on socket to prevent concurrent access
         InputStream input = socket.getInputStream();
+        synchronized (input) {
 
-        int index = 0;
-        byte[] msgBytes = null, tmp = null;
-        byte[] bufferBytes = new byte[BUFFER_SIZE];
+            int index = 0;
+            byte[] msgBytes = null, tmp = null;
+            byte[] bufferBytes = new byte[BUFFER_SIZE];
 
-        /* read first char from stream */
-        byte read = (byte) input.read();
-        boolean reading = true;
+            /* read first char from stream */
+            byte read = (byte) input.read();
+            boolean reading = true;
 
-        while(read != 13 && read != -1 && reading) {/* CR, disconnect, error */
-            /* if buffer filled, copy to msg array */
-            if(index == BUFFER_SIZE) {
-                if(msgBytes == null){
-                    tmp = new byte[BUFFER_SIZE];
-                    System.arraycopy(bufferBytes, 0, tmp, 0, BUFFER_SIZE);
-                } else {
-                    tmp = new byte[msgBytes.length + BUFFER_SIZE];
-                    System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
-                    System.arraycopy(bufferBytes, 0, tmp, msgBytes.length,
-                            BUFFER_SIZE);
+            while(read != 13 && read != -1 && reading) {/* CR, disconnect, error */
+                /* if buffer filled, copy to msg array */
+                if(index == BUFFER_SIZE) {
+                    if(msgBytes == null){
+                        tmp = new byte[BUFFER_SIZE];
+                        System.arraycopy(bufferBytes, 0, tmp, 0, BUFFER_SIZE);
+                    } else {
+                        tmp = new byte[msgBytes.length + BUFFER_SIZE];
+                        System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+                        System.arraycopy(bufferBytes, 0, tmp, msgBytes.length,
+                                BUFFER_SIZE);
+                    }
+
+                    msgBytes = tmp;
+                    bufferBytes = new byte[BUFFER_SIZE];
+                    index = 0;
                 }
 
-                msgBytes = tmp;
-                bufferBytes = new byte[BUFFER_SIZE];
-                index = 0;
+                /* only read valid characters, i.e. letters and constants */
+                bufferBytes[index] = read;
+                index++;
+
+                /* stop reading is DROP_SIZE is reached */
+                if(msgBytes != null && msgBytes.length + index >= DROP_SIZE) {
+                    reading = false;
+                }
+
+                /* read next char from stream */
+                read = (byte) input.read();
             }
 
-            /* only read valid characters, i.e. letters and constants */
-            bufferBytes[index] = read;
-            index++;
-
-            /* stop reading is DROP_SIZE is reached */
-            if(msgBytes != null && msgBytes.length + index >= DROP_SIZE) {
-                reading = false;
+            if(msgBytes == null){
+                tmp = new byte[index];
+                System.arraycopy(bufferBytes, 0, tmp, 0, index);
+            } else {
+                tmp = new byte[msgBytes.length + index];
+                System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+                System.arraycopy(bufferBytes, 0, tmp, msgBytes.length, index);
             }
 
-            /* read next char from stream */
-            read = (byte) input.read();
+            msgBytes = tmp;
+
+            // TODO: might need to refactor this to return a null
+            //       when there's no message to read
+
+            /* Check for empty message indicating a disconnect */
+            if(msgBytes.length < 2) {
+                throw new IOException("Error! Connection lost!");
+            }
+
+            /* build final String */
+            KVMessage msg = new KVMessage(msgBytes);
+            if(msg.getStatus() != IKVMessage.StatusType.WAGWAN) {
+                logger.debug("RECEIVE \t<"
+                        + socket.getInetAddress().getHostAddress() + ":"
+                        + socket.getPort() + ">: '"
+                        + msg + "'");
+            }
+            return msg;
         }
-
-        if(msgBytes == null){
-            tmp = new byte[index];
-            System.arraycopy(bufferBytes, 0, tmp, 0, index);
-        } else {
-            tmp = new byte[msgBytes.length + index];
-            System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
-            System.arraycopy(bufferBytes, 0, tmp, msgBytes.length, index);
-        }
-
-        msgBytes = tmp;
-
-        // TODO: might need to refactor this to return a null
-        //       when there's no message to read
-
-        /* Check for empty message indicating a disconnect */
-        if(msgBytes.length < 2) {
-            throw new IOException("Error! Connection lost!");
-        }
-
-        /* build final String */
-        KVMessage msg = new KVMessage(msgBytes);
-        if(msg.getStatus() != IKVMessage.StatusType.WAGWAN) {
-            logger.debug("RECEIVE \t<"
-                    + socket.getInetAddress().getHostAddress() + ":"
-                    + socket.getPort() + ">: '"
-                    + msg + "'");
-        }
-        return msg;
     }
 
     /**
